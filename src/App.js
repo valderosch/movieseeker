e1ec355aee4432e4ef8c222b5fa090689a2e6b2c
valderosch <phonelix96@gmail.com>
2023-01-25 00:58:06 +0200

    'api'

--- src/App.js
+++ src/App.js
@@ -1,6 +1,19 @@
 import React from "react";
+import { useEffect } from "react";
+
+const API_URL = 'http://www.omdbapi.com?apikey=ddf18bce';
 
 function App() {
+  //Searchbar API
+  const searchMovies = async(title) => {
+    const response = await fetch(`${API_URL}&s=${title}`);
+    const data = await response.json();
+    console.log(data.Search);
+  }
+
+  useEffect(()=> {
+    searchMovies('Batman');
+  },[]);  
   return (
     <h1>App</h1>
   );
